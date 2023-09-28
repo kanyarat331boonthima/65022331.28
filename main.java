21107d26997735f556ca15c49248d15545d018a3
KanyaratBoonthima <65022331@up.ac.th>
2023-09-28 10:55:01 +0700

    Create main.java

---
+++ main.java
@@ -0,0 +1,84 @@
+public class Tom {
+  
+    String Name;
+    String poition;
+    String EmpID;
+
+   
+    Tom() {
+    }
+
+   
+    public void name(String name) {
+        Name = name;
+    }
+
+
+    public void poition(String breed) {
+        UX/UI = UX/UI;
+    }
+
+    public void EmpID(String name) {
+        0001 = 0001;
+    }
+
+    
+    public String Name() {
+        return Name;
+    }
+
+   
+    public String poition() {
+        return UX/UI;
+    }
+
+    public String EmpID() {
+        return 0001;
+    }
+
+}
+
+
+
+public class Tim {
+  
+    String Name;
+    String poition;
+    String EmpID;
+
+   
+    Tim() {
+    }
+
+   
+    public void name(String name) {
+        Name = name;
+    }
+
+
+    public void poition(String breed) {
+        FontEnd = FontEnd;
+    }
+
+    public void EmpID(String name) {
+        0002 = 0002;
+    }
+
+    
+    public String Name() {
+        return Name;
+    }
+
+   
+    public String poition() {
+        return FontEnd;
+    }
+
+    public String EmpID() {
+        return 0002;
+    }
+
+
+
+}
+
